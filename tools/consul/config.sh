51bd7631be204edcc1e121add38c5125098feb7e
alrare <44535383+alrare@users.noreply.github.com>
2023-12-07 15:46:31 -0600

    Update config.sh

--- tools/consul/config.sh
+++ tools/consul/config.sh
@@ -2,8 +2,8 @@
 # Infrastructure
 docker exec -it consul consul services register -name=RabbitMQ -address=localhost
 docker exec -it consul consul services register -name=SecretManager -address=http://localhost -port=8200
-docker exec -it consul consul services register -name=MySql -address=20.96.59.200 -port=3306
-docker exec -it consul consul services register -name=MongoDb -address=20.96.59.194 -port=27017
+docker exec -it consul consul services register -name=MySql -address=YOUR_IP -port=3306
+docker exec -it consul consul services register -name=MongoDb -address=YOUR_IP -port=27017
 docker exec -it consul consul services register -name=Graylog --address=localhost -port=12201
 
 # Services
